6db101aa52729e98710911f1ba5b767dfc844d78
Edward <edward.maesen@gmail.com>
2018-11-08 13:27:20 -0800

    Add user authentication

--- client/src/views/Login.vue
+++ client/src/views/Login.vue
@@ -19,6 +19,7 @@
           required
           autofocus
           v-model="user.username"
+          @change="clearError"
           type="text"
           pattern="[0-9A-Za-z][0-9A-Za-z ]{0,16}[0-9A-Za-z]"
           placeholder=""
@@ -33,6 +34,7 @@
           name="password"
           required
           v-model="user.password"
+          @change="clearError"
           type="password"
           pattern=".{4,18}"
           placeholder=""
@@ -42,17 +44,34 @@
       </div>
       <div class="">
         <input
+          v-if="!loading"
           class="button"
           type="submit"
           value="Login"
           :disabled="!isValid"
-        /> <span class="register">-or- &nbsp;<router-link to="/register">Register</router-link></span>
+        />
+        <input
+          v-if="loading"
+          class="button loading"
+          type="submit"
+          value="in progress..."
+          disabled
+        />
+         <span class="register">-or- &nbsp;<router-link to="/register">Register</router-link></span>
+      </div>
+      <div
+        v-if="authError"
+        class="auth-error"
+        >
+        {{ authError }}
       </div>
     </fieldset>
   </form>
 </template>
 
 <script>
+import { mapState, mapActions } from 'vuex';
+
 export default {
   name: 'Login',
   data: function() {
@@ -61,21 +80,37 @@ export default {
       user: {
         username: '',
         password: ''
-      }
+      },
+      authError: ''
     };
   },
   computed: {
     isValid: function() {
       return this.validUsername() && this.validPassword();
-    }
+    },
+    ...mapState('auth', { loading: 'isAuthenticatePending' })
   },
   methods: {
+    ...mapActions('auth', ['authenticate']),
     login: function(evt) {
       if (this.validForm()) {
-        console.log("submitting form");
         //submit form
+        console.log("submitting login form");
+        this.authenticate({
+          strategy: 'local',
+          username: this.user.username,
+          password: this.user.password
+        }).then(resp => {
+          console.log('logged in: ', resp);
+        }).catch(e => {
+          console.error('Authentication error: ', e);
+          this.authError = e.message;
+        })
       }
       evt.preventDefault();
+    },
+    clearError(){
+      this.authError = '';
     },
     validForm(){
       // form has html5 validation
@@ -112,9 +147,13 @@ input:valid + span + span.validation {
   color: #26b72b;
   visibility: visible;
 }
-.errors {
+.errors,
+.auth-error {
   font-size: .85rem;
   color: #e67d09;
+}
+.auth-error {
+  margin-left: .2em;
 }
 ul {
   margin: 0 -1em 1em 0;
