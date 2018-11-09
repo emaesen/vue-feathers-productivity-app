f4c09529817fc0b5b13098e5f1a06d00e50aebb0
Edward <edward.maesen@gmail.com>
2018-11-09 13:03:12 -0800

    Add support for custom page title and meta tags

--- client/src/router.js
+++ client/src/router.js
@@ -28,7 +28,7 @@ function logOut(to, from, next) {
   });
 }
 
-export default new Router({
+const router = new Router({
   mode: 'history',
   base: process.env.BASE_URL,
   routes: [
@@ -46,6 +46,9 @@ export default new Router({
       component: Register,
       beforeEnter(to, from, next) {
         logOut(to, from, next)
+      },
+      meta: {
+        title: 'Register'
       }
     },
     {
@@ -54,6 +57,9 @@ export default new Router({
       component: Login,
       beforeEnter(to, from, next) {
         logOut(to, from, next)
+      },
+      meta: {
+        title: 'Login'
       }
     },
     {
@@ -63,7 +69,68 @@ export default new Router({
       // this generates a separate chunk (dashboard.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import(/* webpackChunkName: "dashboard" */ './views/Dashboard.vue'),
-      beforeEnter: checkAuth
+      beforeEnter: checkAuth,
+      meta: {
+        title: 'Dashboard'
+      }
     }
   ]
 })
+
+// Hook to set page tile and meta tags
+// (https://alligator.io/vuejs/vue-router-modify-head/)
+// usage:
+/*
+meta: {
+      title: 'My Page Title',
+      metaTags: [
+        {
+          name: 'description',
+          content: 'My description.'
+        },
+        {
+          property: 'og:description',
+          content: 'My description'
+        }
+      ]
+    },
+*/
+// This callback runs before every route change, including on page load.
+router.beforeEach((to, from, next) => {
+  // This goes through the matched routes from last to first, finding the closest route with a title.
+  // eg. if we have /some/deep/nested/route and /some, /deep, and /nested have titles, nested's will be chosen.
+  const nearestWithTitle = to.matched.slice().reverse().find(r => r.meta && r.meta.title);
+
+  // Find the nearest route element with meta tags.
+  const nearestWithMeta = to.matched.slice().reverse().find(r => r.meta && r.meta.metaTags);
+  const previousNearestWithMeta = from.matched.slice().reverse().find(r => r.meta && r.meta.metaTags);
+
+  // If a route with a title was found, set the document (page) title to that value.
+  if(nearestWithTitle) document.title = nearestWithTitle.meta.title;
+
+  // Remove any stale meta tags from the document using the key attribute we set below.
+  Array.from(document.querySelectorAll('[data-vue-router-controlled]')).map(el => el.parentNode.removeChild(el));
+
+  // Skip rendering meta tags if there are none.
+  if(!nearestWithMeta) return next();
+
+  // Turn the meta tag definitions into actual elements in the head.
+  nearestWithMeta.meta.metaTags.map(tagDef => {
+    const tag = document.createElement('meta');
+
+    Object.keys(tagDef).forEach(key => {
+      tag.setAttribute(key, tagDef[key]);
+    });
+
+    // We use this to track which meta tags we create, so we don't interfere with other ones.
+    tag.setAttribute('data-vue-router-controlled', '');
+
+    return tag;
+  })
+  // Add the meta tags to the document head.
+  .forEach(tag => document.head.appendChild(tag));
+
+  next();
+});
+
+export default router;
