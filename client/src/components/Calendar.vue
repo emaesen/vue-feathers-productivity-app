12f9830d156d8d1981a841b3fe96ccdc4b5e0e48
Edward <edward.maesen@gmail.com>
2018-12-18 14:01:05 -0800

    Add preliminary calendar day

--- client/src/components/Calendar.vue
+++ client/src/components/Calendar.vue
@@ -3,17 +3,23 @@
     <h2 class="calendar">
       <font-awesome-icon icon="calendar-alt"/>Calendar
     </h2>
-    <pa-calendar-month/>
+    <transition name="fade" mode="out-in">
+      <pa-calendar-day v-if="dayInFocus" :date="dayInFocus" key="day"/>
+      <pa-calendar-month v-if="!dayInFocus" key="month"/>
+    </transition>
   </section>
 </template>
 
 <script>
 import CalendarMonth from "./CalendarMonth";
+import CalendarDay from "./CalendarDay";
+import { mapGetters } from "vuex";
 
 export default {
   name: "Calendar",
   components: {
-    "pa-calendar-month": CalendarMonth
+    "pa-calendar-month": CalendarMonth,
+    "pa-calendar-day": CalendarDay
   },
   props: {
     onDashboard: {
@@ -23,6 +29,12 @@ export default {
   },
   data() {
     return {};
+  },
+  computed: {
+    ...mapGetters({ calendarState: "calendar" }),
+    dayInFocus() {
+      return this.calendarState.dayInFocus;
+    }
   }
 };
 </script>
@@ -38,5 +50,14 @@ h2.calendar {
   display: inline-block;
   position: relative;
   top: -5px;
+}
+.fade-enter-active,
+.fade-leave-active {
+  transition: all 0.4s ease-in-out, transform 0.4s ease-in-out;
+}
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
+  transform: translateY(-9px);
 }
 </style>
--- client/src/components/CalendarDay.vue
+++ client/src/components/CalendarDay.vue
@@ -1,12 +1,54 @@
-<template></template>
+<template>
+  <div class="container">
+    <button class="action button absolute top right spaced" @click="deFocus">
+      <font-awesome-icon icon="check-circle"/>done
+    </button>
+    <h4>{{ day }}</h4>
+    <div class="day">{{ date }}</div>
+  </div>
+</template>
 
 <script>
+import { mapMutations } from "vuex";
+import calendarUtils from "../utils/calendar";
+
 export default {
+  props: {
+    date: {
+      type: Object
+    }
+  },
   data() {
     return {};
+  },
+  computed: {
+    day() {
+      return calendarUtils.formattedDate(this.date.date, {
+        showYear: true
+      });
+    }
+  },
+  methods: {
+    ...mapMutations(["SET_CALENDAR_DAYINFOCUS"]),
+    deFocus() {
+      this.SET_CALENDAR_DAYINFOCUS(null);
+    }
   }
 };
 </script>
 
 <style scoped>
+.container {
+  position: relative;
+}
+.top {
+  top: -2.8em;
+}
+h4 {
+  text-align: center;
+  margin-top: 0;
+}
+.day {
+  min-height: 300px;
+}
 </style>
