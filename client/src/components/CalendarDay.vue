1c40ce96829800a5fe87fe791d371562b0237941
Edward <edward.maesen@gmail.com>
2019-01-09 12:03:25 -0800

    Fix formatting issue

--- client/src/components/CalendarDay.vue
+++ client/src/components/CalendarDay.vue
@@ -1,6 +1,14 @@
 <template>
   <div class="container">
-    <div v-if="onCalendar">...</div>
+    <div v-if="onCalendar">
+      <div class="reminders">
+        <div v-for="reminder in todaysReminders" :key="reminder.time">
+          <span class="time">{{ reminder.time }}</span>
+          <span class="text">{{ reminder.text }}</span>
+        </div>
+      </div>
+      <div class="events"></div>
+    </div>
     <div v-else>
       <button class="action button absolute top right spaced" @click="deFocus">
         <font-awesome-icon icon="check-circle"/>done
@@ -34,17 +42,45 @@ export default {
   data() {
     return {};
   },
+  mounted() {
+    // console.log("in CalendarDay mounted", {
+    //   day: this.day,
+    //   events: this.events,
+    //   reminders: this.reminders
+    // });
+  },
   computed: {
     day() {
       return calendarUtils.formattedDate(this.date.date, {
         showYear: true
       });
+    },
+    todaysReminders() {
+      let today = calendarUtils.yyyy_mm_dd(this.date.date);
+      let reminders = this.reminders
+        .filter(rem => rem.date === today)
+        .map(rem => {
+          return { text: rem.text, time: rem.time };
+        })
+        .sort(
+          (a, b) => 1 * a.time.replace(":", ".") - 1 * b.time.replace(":", ".")
+        );
+      return reminders;
     }
   },
   methods: {
     ...mapMutations(["SET_CALENDAR_DAYINFOCUS"]),
     deFocus() {
       this.SET_CALENDAR_DAYINFOCUS(null);
+    }
+  },
+  watch: {
+    reminders() {
+      // console.log("in CalendarDay watch reminders", {
+      //   day: this.day,
+      //   events: this.events,
+      //   reminders: this.reminders
+      // });
     }
   }
 };
