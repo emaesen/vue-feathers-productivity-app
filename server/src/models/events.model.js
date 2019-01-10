66388d1e433ae281a61b257b48697e99e9efc048
Edward <edward.maesen@gmail.com>
2019-01-10 11:03:47 -0800

    Add timestampData and schema comment

--- server/src/models/events.model.js
+++ server/src/models/events.model.js
@@ -1,12 +1,26 @@
 const NeDB = require('nedb');
 const path = require('path');
 
-module.exports = function (app) {
+module.exports = function(app) {
   const dbPath = app.get('nedb');
   const Model = new NeDB({
     filename: path.join(dbPath, 'events.db'),
+    timestampData: true,
     autoload: true
   });
 
+  // schema:
+  /*
+  event: {
+    _id: '',
+    ownerId: user._id,
+    title: '',
+    description: '',
+    date: '',
+    time: '',
+    createdAt: {},
+    updatedAt: {}
+  }
+  */
   return Model;
 };
