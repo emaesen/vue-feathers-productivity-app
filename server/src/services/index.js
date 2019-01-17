51d8daf17612816d6a547bdbbdaa77393ea3f14c
Edward <edward.maesen@gmail.com>
2019-01-17 15:20:22 -0800

    Add Feathers todos service

--- server/src/services/index.js
+++ server/src/services/index.js
@@ -2,10 +2,12 @@ const users = require('./users/users.service.js');
 const notes = require('./notes/notes.service.js');
 const reminders = require('./reminders/reminders.service.js');
 const events = require('./events/events.service.js');
+const todos = require('./todos/todos.service.js');
 // eslint-disable-next-line no-unused-vars
 module.exports = function (app) {
   app.configure(users);
   app.configure(notes);
   app.configure(reminders);
   app.configure(events);
+  app.configure(todos);
 };
