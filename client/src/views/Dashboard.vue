d0d299f9881b83fef5aa1e88a88973ec17bd8ca6
Edward <edward.maesen@gmail.com>
2021-03-25 10:36:50 -0700

    switch notes and calendar

--- client/src/views/Dashboard.vue
+++ client/src/views/Dashboard.vue
@@ -5,8 +5,8 @@
     </h1>
     <pa-reminders view="dashboard" :onDashboard="true" class="section"/>
     <pa-todos view="dashboard" :onDashboard="true" class="section"/>
-    <pa-calendar view="dashboard" :onDashboard="true" class="section"/>
     <pa-notes view="dashboard" :onDashboard="true" class="section"/>
+    <pa-calendar view="dashboard" :onDashboard="true" class="section"/>
   </div>
 </template>
 
