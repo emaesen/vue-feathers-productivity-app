17085b4b3d27bdd88e8e9505234399c9b62ad5b2
Edward <edward.maesen@gmail.com>
2021-03-29 12:49:50 -0700

    allow console.log in full build

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -5,8 +5,8 @@ module.exports = {
   },
   extends: ["plugin:vue/essential", "eslint:recommended"],
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
+    "no-console": "off",
+    "no-debugger": "off"
   },
   parserOptions: {
     parser: "babel-eslint"
